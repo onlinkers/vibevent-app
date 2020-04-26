266f5be49f21bb96ab0a20993707e5ed6df0384a
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2020-04-25 19:19:32 -0700

    ignore case

---
+++ src/routes/Authentication/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthenticationWrapper = () => {
+	return (
+		<div>
+            Authentication Wrapper
+		</div>
+	);
+};
+
+export default AuthenticationWrapper;
---
+++ src/routes/Events/EventDashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EventDashboard = () => {
+	return (
+		<div>
+            Event Dashboard
+		</div>
+	);
+};
+
+export default EventDashboard;
