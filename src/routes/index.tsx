82f93d99318d70432708f5b21c33fd1379e68a9c
Chris <chris.791.ix@gmail.com>
2020-05-10 16:58:17 -0700

    started dashboard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,21 +5,23 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Events from "./Event";
 import Discover from "./Discover";
+import Dashboard from "./Dashboard";
 import Auth from "./Authentication";
 import NotFound from "./Authentication/NotFound";
 
 const Routes = () => {
-	return (
-		<BrowserRouter>
-			<Switch>
-				<Route exact path="/" component={Home}/>
-				<Route path="/discover" component={Discover}/>
-				<Route path="/event" component={Events}/>
-				<Route path="/auth" component={Auth}/>
-				<Route path="*" component={NotFound}/>
-			</Switch>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/discover" component={Discover} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/event" component={Events} />
+        <Route path="/auth" component={Auth} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default Routes;
