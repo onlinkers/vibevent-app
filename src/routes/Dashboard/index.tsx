4557471a37ab4200ba1c1850152dc3eda8f8cd55
Chris <chris.791.ix@gmail.com>
2020-05-25 20:22:28 -0700

    connected dashboard to redux

--- src/routes/Dashboard/index.tsx
+++ src/routes/Dashboard/index.tsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { connect } from "react-redux";
+import { EventListLoading } from "types/store";
 
-const Dashboard = () => {
-  return <div>dashboard</div>;
+interface DispatchProps {
+  eventData: EventListLoading;
+}
+
+const Dashboard: React.FunctionComponent<DispatchProps> = ({ eventData }) => {
+  console.log(eventData);
+  const eventList = eventData.events.map((event) => {
+    return (
+      <li key={event._id}>
+        {event.name}
+        <ul>
+          <li>{event.description}</li>
+          <li>{event.venue.name}</li>
+          <li>{event.endDate}</li>
+        </ul>
+      </li>
+    );
+  });
+  return (
+    <div>
+      <div>dashboard</div>
+      <ul>{eventList}</ul>
+    </div>
+  );
 };
 
-export default Dashboard;
+const mapStateToProps = ({ eventData }) => {
+  return {
+    eventData: eventData,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
