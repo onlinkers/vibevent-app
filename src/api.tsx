30f0a580ca1ee3316fa26a786cd2cb73900519fe
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2020-06-10 01:53:14 -0700

    interceptor

--- src/api.tsx
+++ src/api.tsx
@@ -10,10 +10,10 @@ const axiosInstance = axios.create({
   },
 });
 
+// Response Interceptor
 axiosInstance.interceptors.response.use((response) => {
-  // Do something with response data
 
-  // Netlify error where API calls return 200 status but no data
+  // Deal with Netlify error where API calls return 200 status but no data
   if(typeof response.data === "string" && response.data.includes("doctype")) {
     throw ({
       response: {
@@ -30,6 +30,16 @@ axiosInstance.interceptors.response.use((response) => {
 }, (error) => {
   // Do something with response error
   return Promise.reject(error);
+});
+
+// Request Interceptor
+axiosInstance.interceptors.request.use(function (config) {
+  // get the id token from local storage
+  const rawToken = localStorage.getItem("cognitoIdToken");
+  const token = `Bearer ${rawToken}`;
+  config.headers.Authorization =  token;
+
+  return config;
 });
 
 export default axiosInstance;
