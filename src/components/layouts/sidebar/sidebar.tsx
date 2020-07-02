36bd8e0189c3f1266b02b72eb18b5de2f89e7938
Chris <chris.791.ix@gmail.com>
2020-07-02 01:23:52 -0700

    animated sidebar

--- src/components/layouts/sidebar/sidebar.tsx
+++ src/components/layouts/sidebar/sidebar.tsx
@@ -4,27 +4,66 @@ import MenuToggle from "components/svg/MenuToggle";
 
 import { motion, useCycle } from "framer-motion";
 import useDimensions from "react-use-dimensions";
+import { NONAME } from "dns";
 
 interface Props {}
 
-const variants = {
+const sidebarVariants = {
   open: (height = 1000) => ({
-    clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+    width: "15%",
     transition: {
       type: "spring",
-      stiffness: 20,
+      stiffness: 300,
       restDelta: 2,
+      damping: 100,
     },
   }),
-  closed: {
-    clipPath: "circle(30px at 40px 40px)",
+  closed: () => ({
+    width: "5%",
     transition: {
-      delay: 0.5,
+      delay: 0,
       type: "spring",
-      stiffness: 400,
-      damping: 40,
+      stiffness: 300,
+      damping: 100,
+    },
+  }),
+};
+
+const navVariants = {
+  open: {
+    "pointer-events": "auto",
+    x: 50,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 },
     },
   },
+  closed: {
+    "pointer-events": "none",
+    x: 0,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+  },
+};
+
+const links = ["Dashboard", "Discover"];
+
+const Navigation = ({ list }) => {
+  return (
+    <motion.ul className="navlinks">
+      {list.map((item) => {
+        return (
+          <>
+            <motion.li variants={navVariants} className="navlink">
+              {item}
+            </motion.li>
+          </>
+        );
+      })}
+    </motion.ul>
+  );
 };
 
 const Sidebar: React.FunctionComponent<Props> = () => {
@@ -37,7 +76,11 @@ const Sidebar: React.FunctionComponent<Props> = () => {
         className="sidebar"
         initial={false}
         animate={isOpen ? "open" : "closed"}
+        custom={height}
+        ref={containerRef}
+        variants={sidebarVariants}
       >
+        <Navigation list={links} />
         <MenuToggle toggle={() => toggleOpen()} />
       </motion.nav>
     </>
