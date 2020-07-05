3c90424b9191f3f5fb25e34fa440a23bea641f42
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2020-07-05 01:14:16 -0700

    event details draft

--- src/routes/Event/EventDetails/index.tsx
+++ src/routes/Event/EventDetails/index.tsx
@@ -1,17 +1,71 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-const EventDetails = () => {
+import { Button } from "antd";
+
+import { EventsPayload, EventCategoriesPayload } from "types/store";
+
+interface Props {
+  events: EventsPayload;
+  eventCategories: EventCategoriesPayload;
+  loading: boolean;
+  errors: {
+    events?: string
+    eventCategories?: string,
+  };
+}
+
+const EventDetails: React.FunctionComponent<Props> = (props) => {
+
+  const {
+    events,
+    eventCategories,
+    loading,
+    errors
+  } = props;
 
   const { eventId } = useParams();
+  
+  const event = events[eventId];
 
-  // TODO: Get information about the single event
-
-  return (
+  return event ? (
     <div>
-            Event Details for event: {eventId}
+      Event Details for event: {eventId}
+      <h1>{event.name}</h1>
+      <p>{event.description}</p>
+      <div>
+        <div>{event.startDate} - {event.endDate}</div>
+        <div>{event.venue.name}</div>
+      </div>
+      Categories:
+      <ul>
+        {event.categories.map((category) =>
+          <li key={category} className="t--capitalize">{eventCategories[category]}</li>
+        )}
+      </ul>
+      Rooms:
+      <ul>
+        {event.rooms?.map((room) =>
+          <li key={room.roomId}>
+            <Link to={`/event/${eventId}/room/${room.roomId}`} target="_blank">
+              <Button>{room.name || "Event Room"}</Button>
+            </Link>
+          </li>
+        )}
+      </ul>
     </div>
-  );
+  ) : <div>Event not found</div>;
 };
 
-export default EventDetails;
+
+const mapStateToProps = ({ eventData }) => {
+  return {
+    events: eventData.events,
+    eventCategories: eventData.eventCategories,
+    loading: eventData.loading,
+    errors: eventData.errors
+  };
+};
+
+export default connect(mapStateToProps, {})(EventDetails);
