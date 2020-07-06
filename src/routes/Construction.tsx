0542fba5a4f06df03fe2b7a3c5f0771938e91dd5
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2020-07-06 15:17:13 -0700

    better page styles

--- src/routes/Construction.tsx
+++ src/routes/Construction.tsx
@@ -7,7 +7,7 @@ import Sidebar from "components/layouts/sidebar/sidebar";
 const Construction = () => {
   const history = useHistory();
   return (
-    <div className="Page--center" style={{ paddingLeft: "10%", flexDirection: "column" }}>
+    <div className="Page--center" style={{ flexDirection: "column" }}>
       <Sidebar />
       <Empty description={false}>
         Page is Under Construction!!
