be29b7d95ad27cc9489b0ef52f3a3a19da9f4285
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2020-07-06 19:48:26 -0700

    room init

--- src/routes/Rooms/index.tsx
+++ src/routes/Rooms/index.tsx
@@ -21,8 +21,45 @@ const Room: React.FunctionComponent<Props> = () => {
 
     // eslint-disable-next-line
     console.log("roomId", { roomId });
-    setLoaded(true);
-    if(!roomId) setError(true);
+    if(!roomId) {
+      setError(true);
+      setLoaded(true);
+    }
+    else {
+      setLoaded(true);
+      const script = document.createElement("script");
+      script.innerHTML = `
+        var _options = {
+            '_license_key':'${roomId}',
+            '_role_token':'',
+            '_registration_token':'',
+            '_widget_containerID':'embedWidget',
+            '_widget_width':'100%',
+            '_widget_height':'100vh',
+        };
+        (function() {
+            !function(i){
+                i.Widget=function(c){
+                    'function'==typeof c&&i.Widget.__cbs.push(c),
+                    i.Widget.initialized&&(i.Widget.__cbs.forEach(function(i){i()}),
+                    i.Widget.__cbs=[])
+                },
+                i.Widget.__cbs=[]
+            }(window);
+            var ab = document.createElement('script'); 
+            ab.type = 'text/javascript'; 
+            ab.async = true;
+            ab.src = 'https://embed.livewebinar.com/em?t='+_options['_license_key']+'&'+Object.keys(_options).reduce(function(a,k){
+                    a.push(k+'='+encodeURIComponent(_options[k]));
+                    return a
+                },[]).join('&');
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ab, s);
+        })();`;
+
+      script.async = true;
+      document.body.appendChild(script);
+
+    }
 
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
@@ -38,7 +75,7 @@ const Room: React.FunctionComponent<Props> = () => {
         error ? (
           <NotFound type="room"/>
         ) : (
-          <>Entering room with id {roomId}</>
+          <div id="embedWidget"></div>
         )
       )}
     </div>
