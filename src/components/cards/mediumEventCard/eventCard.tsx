a04ba112892784ef894bb4a898504d90d1bf1766
Chris <chris.791.ix@gmail.com>
2020-07-20 19:41:37 +0700

    fixed incorrect font sizes on different screens, readjusted small card width and height, fixed share and save button positioning on medium cards

--- src/components/cards/mediumEventCard/eventCard.tsx
+++ src/components/cards/mediumEventCard/eventCard.tsx
@@ -1,10 +1,8 @@
-import React, { useContext } from "react";
+import React from "react";
 import moment from "moment";
-import { motion } from "framer-motion";
 
 import "./index.scss";
 import { Event } from "types/props";
-import { ThemeContext } from "context/ThemeContext";
 
 import shareIcon from "../../../assets/icons/share.svg";
 import bookmarkIcon from "../../../assets/icons/bookmark.svg";
@@ -15,18 +13,11 @@ interface Props {
 }
 
 const MediumEventCard: React.FunctionComponent<Props> = (props) => {
-  const { breakpoint } = useContext(ThemeContext);
   const { event, onClick = () => {} } = props;
   const month = moment(event?.startDate).format("MMM DD");
   return (
     <>
-      <motion.div
-        className="medium-card"
-        whileHover={breakpoint === "desktop" ? { scale: 1.1 } : {}}
-        whileTap={{ scale: 1 }}
-        transition={{ duration: 0.001 }}
-        onClick={() => onClick()}
-      >
+      <div className="medium-card" onClick={() => onClick()}>
         <img
           className="medium-card-coverphoto"
           src={event?.media?.coverPhoto?.baseSrc}
@@ -35,22 +26,19 @@ const MediumEventCard: React.FunctionComponent<Props> = (props) => {
         <div className="medium-card-description">
           <div className="description-text">
             <p className="description-date">{month}</p>
-            <p className="description-title">
-              {event?.name}
-            </p>
+            <p className="description-title">{event?.name}</p>
             <p className="description-host">UBC SISA</p>
           </div>
           <div className="description-options">
             <a className="share-button">
-              <img src={shareIcon}/>
+              <img src={shareIcon} />
             </a>
             <a className="bookmark-button">
-              <img src={bookmarkIcon}/>
+              <img src={bookmarkIcon} />
             </a>
           </div>
         </div>
-
-      </motion.div>
+      </div>
     </>
   );
 };
