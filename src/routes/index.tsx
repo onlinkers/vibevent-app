3ffa5c8924b116aa1aa0621a07d1cb6a702a8bac
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2020-09-09 01:56:12 -0700

    hotfix -- redirect unused in routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext } from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { connect } from "react-redux";
 import { Auth } from "aws-amplify";
 
